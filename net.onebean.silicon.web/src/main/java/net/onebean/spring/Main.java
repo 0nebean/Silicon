2b1679cb343c3b84f36553b437a3790f2cc98e1d
0neBean <pein790751571@aliyun.com>
2020-03-25 23:18:42 +0800

    no message

--- net.onebean.silicon.web/src/main/java/net/onebean/spring/Main.java
+++ net.onebean.silicon.web/src/main/java/net/onebean/spring/Main.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 
+
 @ComponentScan(
 		basePackages = {
 				"net.onebean.**.service",
